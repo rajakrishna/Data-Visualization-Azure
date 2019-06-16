bbe626ebb1399eab2b3912e8b4060c74d28e12b5
Raja-Krishna <rajakrishna214@gmail.com>
2019-06-16 00:55:46 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,18 @@
 from flask import Flask
 import os
+import pyodbc
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Yay yes it is working"
+   
+    server = '<server>.database.windows.net'
+    database = '<database>'
+    username = '<username>'
+    password = '<password>'
+    driver= '{ODBC Driver 17 for SQL Server}'
+    cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT count(*) from all_month")
+    row = cursor.fetchone()
+    return row
