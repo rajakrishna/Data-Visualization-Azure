09416fb59cc15f9079dcecbfc4c2ef08870f074a
Raja-Krishna <rajakrishna214@gmail.com>
2019-06-15 21:27:06 -0500

    Update application.py

--- application.py
+++ application.py
@@ -5,8 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
-
-port = int(os.getenv("PORT", 5000))
-if __name__ == '__main__':
-  app.run(debug="true",port=port)
-#    app.run("0.0.0.0",port=port)
